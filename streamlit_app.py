2325a140fcf90b68f1efa7f9110f449ee7dc9045
javittoOliver <117694796+javittoOliver@users.noreply.github.com>
2024-12-09 16:02:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,7 +163,7 @@ def mostrar_resultados(recomendacion):
         st.markdown(
         f"""
         <div style="background-color: #FDEBD9; padding: 10px; border-radius: 7px; color: #606062;
-        font-size: 18px; font-weight: 600; line-height: 1;">
+        font-size: 16px; font-weight: 600; line-height: 1;">
         <br>{recomendacion_html}<br>
         <br>
         </div>
